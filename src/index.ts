02632ae1d04379b579084703ba196e3445306984
Vazir Vadirov <ggvalion@gmail.com>
2024-11-26 18:56:13 +0300

    Rewritten most of logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import {Bot, GrammyError, HttpError, InlineKeyboard, Keyboard} from "grammy";
 import dotenv from "dotenv";
-import {getQuizByName} from "./service/fetcher";
+import {getAnswer, getQuizByName} from "./service/QuestionService";
+import Question from "./dto/Question";
 
-console.log(dotenv.config())
+dotenv.config()
 const bot = new Bot(process.env.TOKEN as string);
 
 const topics = [
@@ -20,31 +21,78 @@ bot.command("start", async (ctx) => {
 
     await ctx.reply("Hello! Welcome to the IT Quiz!📃");
     await ctx.reply("Select the topic that you want to test 👇", ({
-        reply_markup: startKeyboard
+        reply_markup: startKeyboard.resized()
     }));
 })
+
+const userSessions: Record<number, { topic: string; currentIndex: number; score: number; quiz: Question[], messageId: number | null }> = {};
 
 bot.hears(topics.flat(), async (ctx) => {
     const topic = ctx?.message?.text?.toLowerCase()!
     const questions = await getQuizByName(topic)
+    userSessions[ctx.chatId] = { topic: topic, currentIndex: 0, score: 0, quiz: questions, messageId: null }
 
-    const buttonRows = questions[0].options
+    await ctx.reply(`Starting the quiz on: ${topic}`)
+    await sendQuestion(ctx, ctx.chatId)
+})
+
+async function sendQuestion(ctx: any, chatId: number) {
+    const session = userSessions[chatId]
+    const { quiz, currentIndex } = session
+    const question = quiz[currentIndex]
+
+    const buttonRows = question.options
         .filter(option => option.text !== null)
         .map(option => [
-        InlineKeyboard.text(
-            option?.text!!,
-            JSON.stringify({
-                type: `${ctx?.message?.text?.toLowerCase()!!}-option`,
-                isCorrect: option.isCorrect,
-                questionId: questions[0].id,
-            })
-        )
-    ])
+            InlineKeyboard.text(
+                option?.text!!,
+                JSON.stringify({
+                    type: `${ctx?.message?.text?.toLowerCase()!!}-option`,
+                    isCorrect: option.isCorrect,
+                    questionId: question.id
+                })
+            )
+        ])
+
+    console.log(question.options)
 
     let inlineKeyboard = InlineKeyboard.from(buttonRows)
-    await ctx.reply(questions[0].text, {
+    const sentMessage = await ctx.reply(question.text, {
         reply_markup: inlineKeyboard,
     });
+    session.messageId = sentMessage.message_id
+}
+
+bot.on('callback_query:data', async (ctx) => {
+    const session = userSessions[ctx?.chatId!!]
+    const { quiz, messageId } = session;
+    const callbackData = JSON.parse(ctx?.callbackQuery?.data)
+    const answer = getAnswer(callbackData.questionId)
+
+    if (messageId) {
+        await ctx.api.deleteMessage(ctx?.chatId!!, messageId)
+    }
+
+    if (callbackData.isCorrect == "true") {
+        session.currentIndex++;
+        session.score++
+        await ctx.reply("Right!")
+    } else {
+        if (answer !== null) {
+            await ctx.reply(`Wrong Answer! \nThe question: ${quiz[session.currentIndex].text} \nRight answer: ${answer}`)
+        } else {
+            await ctx.reply("Wrong Answer!")
+        }
+        session.currentIndex++;
+    }
+
+    if (session.currentIndex >= quiz.length) {
+        await ctx.reply(`Quiz over! You scored ${session.score} out of ${quiz.length}.`)
+        delete userSessions[ctx?.chatId!!]
+    } else {
+        await sendQuestion(ctx, ctx?.chatId!!)
+        await ctx.answerCallbackQuery()
+    }
 })
 
 bot.catch((err) => {
