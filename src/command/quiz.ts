bfea8068fdbb26cb70f094a959c62dba44ebd96c
Vazir Vadirov <ggvalion@gmail.com>
2024-12-02 13:49:24 +0500

    Renaming

--- src/command/quiz.ts
+++ src/command/quiz.ts
@@ -4,12 +4,12 @@ import {topics} from "./types";
 const composer = new Composer();
 
 composer.command("quiz", async (ctx) => {
-    const startKeyboard = topics.reduce((keyboard, row) => {
+    const quizKeyboard = topics.reduce((keyboard, row) => {
         row.forEach(text => keyboard.text(text))
         return keyboard.row();
     }, new Keyboard())
     await ctx.reply("Select the topic on keyboard for starting quiz 👇", {
-        reply_markup: startKeyboard.resized(),
+        reply_markup: quizKeyboard.resized(),
     })
 })
 
