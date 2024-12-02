357df22c16b09d95cfbd947dc32a0014c9307fae
Vazir Vadirov <ggvalion@gmail.com>
2024-12-02 14:31:01 +0500

    Data from .env

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -4,11 +4,11 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "",
-    database: "quiz",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT!),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
